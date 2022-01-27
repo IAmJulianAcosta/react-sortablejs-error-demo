55e6c929b7a8d6e66fbdf5cf3bf87b48d16d24d5
Julián Acosta <v-juliana@microsoft.com>
2022-01-27 11:58:12 -0500

    Add demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {ReactSortable} from "react-sortablejs";
+
+class Demo {
+  name: string;
+  id: number;
+
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
 
 function App() {
+  const [demos, setDemos] = useState<Demo[]>([
+    new Demo('one', 1),
+    new Demo('two', 2),
+    new Demo('three', 3),
+  ]);
+
+  function classDiv(demo: Demo) {
+    return <div style={{background: 'red'}} key={demo.id}>{demo.name}</div>;
+  }
+
+  function nonClassDiv(demo: Demo) {
+    return <div style={{background: 'blue'}} key={demo.id}>{demo.name}</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactSortable
+        list={demos}
+        setList={setDemos}
+      >
+        {demos.map((demo: Demo | any) => {
+          console.log(demo);
+          return demo instanceof Demo ? classDiv(demo) : nonClassDiv(demo)
+        })}
+      </ReactSortable>
     </div>
   );
 }
